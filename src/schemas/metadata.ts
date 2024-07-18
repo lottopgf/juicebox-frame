e254aecd4591c2fb36afd155c69ecef7e90c1deb
maggo <me@mar.co.de>
2024-07-18 16:43:47 +0200

    feat: migrate to unified metadata schema

---
+++ src/schemas/metadata.ts
@@ -0,0 +1,43 @@
+import { any, array, object, optional, string } from "valibot";
+
+/**
+ * Compatible metadata schema.
+ * @see https://docs.juicebox.money/dev/frontend/metadata/#version-9
+ */
+export const MetadataSchema = object({
+  name: string(),
+  logoUri: optional(string()),
+  infoUri: string(),
+  description: string(),
+  /** Introduced in V2 */
+  payText: optional(string()),
+  payButton: optional(string()),
+  tokens: optional(
+    array(
+      object({
+        value: string(),
+        type: string(),
+      })
+    )
+  ),
+  /** Introduced in V3 */
+  twitter: optional(string()),
+  discord: optional(string()),
+  payDisclosure: optional(string()),
+  /** Introduced in V5 */
+  nftPaymentSuccessModal: optional(
+    object({
+      ctaText: string(),
+      ctaLink: string(),
+      content: string(),
+    })
+  ),
+  /** Introduced in V6 */
+  telegram: optional(string()),
+  /** Introduced in V7 */
+  coverImageUri: optional(string()),
+  /** Introduced in V8 */
+  tags: optional(array(string())),
+  /** Introduced in V9 */
+  projectTagline: optional(string()),
+});
