3d83018be725a715444463a976d0df90a5f1c733
maggo <me@mar.co.de>
2024-07-27 13:39:07 +0400

    feat: implement chart design

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,6 +1,7 @@
 "use server";
 
 import * as d3 from "d3";
+import { fromUnixTime } from "date-fns";
 import { JSDOM } from "jsdom";
 
 const { document } = new JSDOM("").window;
@@ -12,17 +13,13 @@ interface DataPoint {
 }
 
 export async function renderChart(data: DataPoint[]) {
-  // Declare the chart dimensions and margins.
-  const width = 940;
-  const height = 760;
-  const margin = 20;
+  const width = 920;
+  const height = 750;
+  const margin = 50;
 
   const svg = d3
     .create("svg")
-    .attr(
-      "viewBox",
-      `0 0 ${width + margin + margin} ${height + margin + margin}`,
-    );
+    .attr("viewBox", `0 0 ${width + margin * 1.5} ${height + margin * 1.5}`);
 
   const wrapper = svg
     .append("g")
@@ -30,21 +27,80 @@ export async function renderChart(data: DataPoint[]) {
 
   const x = d3
     .scaleUtc()
-    .domain(d3.extent(data, (d) => new Date(d.timestamp)) as [Date, Date])
-    .range([0, width]);
+    .domain(d3.extent(data, (d) => fromUnixTime(d.timestamp)) as [Date, Date])
+    .range([0, width])
+    .nice();
 
-  wrapper
+  const xGroup = wrapper
     .append("g")
     .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).ticks(7));
+    .call(
+      d3
+        .axisBottom<Date>(x)
+        .tickValues(x.ticks(7).concat(x.domain().slice(-1)))
+        .tickFormat(d3.utcFormat("%-m/%-d"))
+        .tickSizeOuter(0),
+    );
+
+  xGroup.selectAll(".tick").each((d, i, nodes) => {
+    // @ts-ignore
+    const tick = d3.select<SVGGElement>(nodes[i]);
+    const textNode = tick.select("text");
+    const [, rawX, rawY] =
+      tick.attr("transform").match(/translate\(([\d.]*),([\d.]*)\)/) ?? [];
+    const text = textNode.text();
+
+    const x = parseFloat(rawX) + margin;
+    const y = parseFloat(rawY) + margin + height;
+
+    tick
+      .append("div")
+      .attr(
+        "tw",
+        `absolute flex justify-center text-2xl text-center w-[100px] -ml-[50px] mt-[5px] top-[${y}px] left-[${x}px]`,
+      )
+      .text(text);
+    textNode.remove();
+  });
 
   // Add Y axis
   const y = d3
     .scaleLinear()
     .domain(d3.extent(data, (d) => d.value) as [number, number])
-    .range([height, 0]);
+    .range([height, 0])
+    .nice(7);
 
-  wrapper.append("g").call(d3.axisLeft(y).ticks(5));
+  const yGroup = wrapper
+    .append("g")
+    .call(d3.axisLeft(y).ticks(7, ".1~f").tickSizeOuter(0))
+    .call((g) =>
+      g
+        .selectAll(".tick line")
+        .clone()
+        .attr("x2", width)
+        .attr("stroke-opacity", 0.2),
+    );
+
+  yGroup.selectAll(".tick").each((d, i, nodes) => {
+    // @ts-ignore
+    const tick = d3.select<SVGGElement>(nodes[i]);
+    const textNode = tick.select("text");
+    const [, , rawY] =
+      tick.attr("transform").match(/translate\(([\d.]*),([\d.]*)\)/) ?? [];
+    const text = textNode.text();
+
+    const x = 0;
+    const y = parseFloat(rawY) + margin;
+
+    tick
+      .append("div")
+      .attr(
+        "tw",
+        `absolute flex justify-end text-2xl text-right w-[50px] -ml-[${margin / 3}px] -mt-[22px] top-[${y}px] left-[${x}px]`,
+      )
+      .text(text);
+    textNode.remove();
+  });
 
   // Add the line
   wrapper
@@ -52,32 +108,16 @@ export async function renderChart(data: DataPoint[]) {
     .datum(data)
     .attr("fill", "none")
     .attr("stroke", "orange")
-    .attr("stroke-width", 10)
+    .attr("stroke-width", 12)
+    .attr("stroke-linecap", "round")
     .attr(
       "d",
       d3
         .line<DataPoint>()
         .curve(d3.curveMonotoneX)
-        .x((d) => x(d.timestamp))
+        .x((d) => x(fromUnixTime(d.timestamp)))
         .y((d) => y(d.value)),
     );
-
-  // Remove all text elements as they're not supported by satori
-  svg.selectAll("text").each(function (d, i, nodes) {
-    const textNode = d3.select(this);
-    // const x = textNode.attr("x");
-    // const y = textNode.attr("y");
-    // const text = textNode.text();
-
-    // // @ts-ignore
-    // d3.select(this.parentNode)
-    //   .append("div")
-    //   .style("top", y)
-    //   .style("left", x)
-    //   .text(text);
-
-    textNode.remove();
-  });
 
   return svg.node()?.outerHTML;
 }
