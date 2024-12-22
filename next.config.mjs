c13e5d5386b6111f621adedb8c1c4c87ccedd2f2
maggo <me@mar.co.de>
2024-12-22 02:58:05 +0100

    fix: eslint and build errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,22 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "jbm.infura-ipfs.io" }],
   },
+  redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://juicebox.money",
+        permanent: true,
+      },
+    ];
+  },
   webpack(config) {
     config.externals.push("pino-pretty", "encoding");
     return config;
+  },
+  experimental: {
+    // Required to make next dev tools happy :^)
+    turbo: {},
   },
 };
 
