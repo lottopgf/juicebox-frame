7a10eadcbb1db419325467c56b1866147b874492
maggo <me@mar.co.de>
2024-12-22 02:35:54 +0100

    feat: upgrade nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "jbm.infura-ipfs.io" }],
   },
+  webpack(config) {
+    config.externals.push("pino-pretty", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
