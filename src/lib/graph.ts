6808cf7fbd15dd50c8dc6ac534e8f225208ce745
maggo <me@mar.co.de>
2025-02-18 13:41:46 +0100

    fix: subgraph url

--- src/lib/graph.ts
+++ src/lib/graph.ts
@@ -2,6 +2,6 @@ import { GraphQLClient } from "graphql-request";
 
 const JUICEBOX_GRAPH_API_KEY = process.env.JUICEBOX_GRAPH_API_KEY!;
 
-const JUICEBOX_GRAPH_URL = `https://subgraph.satsuma-prod.com/${JUICEBOX_GRAPH_API_KEY}/juicebox/mainnet/api`;
+const JUICEBOX_GRAPH_URL = `https://subgraph.satsuma-prod.com/${JUICEBOX_GRAPH_API_KEY}/juicebox/ethereum-v3/api`;
 
 export const graphClient = new GraphQLClient(JUICEBOX_GRAPH_URL);
